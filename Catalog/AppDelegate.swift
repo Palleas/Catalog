92692d7b8ef5340535af0a708134f5a0265a6967
Romain Pouclet <palleas@gmail.com>
2015-01-16 21:46:59 -0500

    Load content of file
    This is fun but it's not going to work very well on a 1+Gb file

--- Catalog/AppDelegate.swift
+++ Catalog/AppDelegate.swift
@@ -9,10 +9,11 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSStreamDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    var stream: NSStream!
+    var buffer: NSMutableData!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
 //        let u = NSURL(string: "http://donnees.ville.montreal.qc.ca/dataset/37450231-e4d4-4e9c-99b3-5e88afa6e053/resource/67a95c30-4e21-4346-83f7-491d4ca54a7e/download/cataloguebibliotheque.zip")!
@@ -20,7 +21,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let task = NSURLSession.sharedSession().downloadTaskWithURL(u, completionHandler: { (url, response, error) -> Void in
             println("Download is over")
-            let csv = self.unzipAndFindCSV(url.path!)
+            if let csv = self.unzipAndFindCSV(url.path!) {
+                println("Found CSV file at \(csv)")
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.importFile(csv)
+                })
+            }
         })
         task.resume()
         
@@ -28,6 +35,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func importFile(path: String) {
+        println("Importing file at path \(path)")
+        
+        // Opening CSV file
+        let stream = NSInputStream(fileAtPath: path)!
+        stream.delegate = self
+        stream.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+        stream.open()
     }
 
     func unzipAndFindCSV(file: String) -> String? {
@@ -41,7 +58,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let task = NSTask()
         task.launchPath = "/usr/bin/env"
-        task.arguments = ["unzip", file, "-d", target]
+        task.arguments = ["unzip", "-o", file, "-d", target]
         task.launch()
         task.waitUntilExit()
         
@@ -50,12 +67,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             return nil
         }
         
-        return NSFileManager.defaultManager().enumeratorAtPath(target)?.allObjects.filter({file -> Bool in
+        let csv = NSFileManager.defaultManager().enumeratorAtPath(target)?.allObjects.filter({file -> Bool in
             println("Current file in extracted folder is \(file)")
 
             return (file as String).pathExtension == "csv"
         }).first as NSString?
+        
+        return csv != nil ? target.stringByAppendingPathComponent(csv!) : nil
     }
     
+    func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
+        switch eventCode {
+        case NSStreamEvent.OpenCompleted:
+            println("Stream has opened")
+            buffer = NSMutableData()
+            break
+        case NSStreamEvent.EndEncountered:
+            println("Stream has reached end")
+            break
+        case NSStreamEvent.HasBytesAvailable:
+            println("Stream has bytes available")
+            
+            let bufferSize = 1024
+            var localBuffer = Array<UInt8>(count: bufferSize, repeatedValue: 0)
+            
+            let read = (aStream as NSInputStream).read(&localBuffer, maxLength: bufferSize)
+            println("Read \(read) bytes")
+            buffer.appendBytes(localBuffer, length: localBuffer.count)
+            
+            break
+        default:
+            println("Not handling stream event: \(eventCode)")
+            
+            
+        }
+    }
 }
 
